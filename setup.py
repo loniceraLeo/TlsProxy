4fd1fc974e91b783ea6cc257b4419068d731c0f9
loniceraLeo <leomasearch@gmail.com>
2021-03-23 23:11:25 +0800

    del

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(
     name = 'TlsProxy',
     version = '0.2.0',
+    descriptor = 'a safe http proxy over tls',
+    long_description = 'README.md',
     author = 'github.com/loniceraLeo',
+    url = 'https://www.github.com/loniceraLeo/TlsProxy',
     packages = ['TlsProxy'],
+    license = 'GPL',
     entry_points = {
         'console_scripts':
             [
